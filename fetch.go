620ff5f4649a1a3c564403b1cd1390e04da3b809
Szabó Ármin András <szabo.armin.andras@gmail.com>
2023-03-21 16:52:04 +0100

    fix generics

--- fetch.go
+++ fetch.go
@@ -2,19 +2,26 @@ package godab
 
 import "database/sql"
 
-func FetchRowsTo(rows sql.Rows, a *[]any) (error, []any) {
-	var result []any
-
+func FetchRowsTo[T any](rows *sql.Rows, result *[]T) error {
 	for rows.Next() {
-		var row any
+		var row T
 
 		err := rows.Scan(&row)
 		if err != nil {
-			return err, nil
+			return err
 		}
 
-		result = append(result, row)
+		*result = append(*result, row)
 	}
 
-	return nil, result
+	return nil
+}
+
+func FetchRowTo[T any](rows *sql.Row, result *T) error {
+	err := rows.Scan(&result)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
