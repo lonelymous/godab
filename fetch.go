496ae7a261d6fc1557e6534d304525ead21ad834
Szabó Ármin András <szabo.armin.andras@gmail.com>
2023-03-22 20:00:43 +0100

    add Struct, Slice and Map to Fetch

--- fetch.go
+++ fetch.go
@@ -1,8 +1,12 @@
 package godab
 
-import "database/sql"
+import (
+	"database/sql"
 
-func FetchRowsTo[T any](rows *sql.Rows, result *[]T) error {
+	"github.com/jmoiron/sqlx"
+)
+
+func FetchRowsToAny[T any](rows *sql.Rows, result *[]T) error {
 	for rows.Next() {
 		var row T
 
@@ -17,8 +21,53 @@ func FetchRowsTo[T any](rows *sql.Rows, result *[]T) error {
 	return nil
 }
 
-func FetchRowTo[T any](rows *sql.Row, result *T) error {
+func FetchRowToAny[T any](rows *sql.Row, result *T) error {
 	err := rows.Scan(&result)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FetchXRowsToStruct[T any](rows *sqlx.Rows, result *[]T) error {
+	for rows.Next() {
+		var row T
+
+		err := rows.StructScan(&row)
+		if err != nil {
+			return err
+		}
+
+		*result = append(*result, row)
+	}
+
+	return nil
+}
+
+func FetchXRowToStruct[T any](rows *sqlx.Row, result *T) error {
+	err := rows.StructScan(&result)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FetchXRowsToSlice[T any](rows *sqlx.Rows, result *[]interface{}) error {
+
+	var err error
+	*result, err = rows.SliceScan()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FetchXRowsToMap[T any](rows *sqlx.Rows, result map[string]interface{}) error {
+
+	err := rows.MapScan(result)
 	if err != nil {
 		return err
 	}
