1aa4138c7591a74e6c01ea813aa0801abf4dc212
Szabó Ármin András <46499638+lonelymous@users.noreply.github.com>
2023-05-07 16:38:45 +0200

    Create Open function

--- database.go
+++ database.go
@@ -1,8 +1,8 @@
 package godab
 
 import (
-	"github.com/jmoiron/sqlx"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 var Database *sqlx.DB
@@ -11,6 +11,11 @@ var Database *sqlx.DB
 // Open connection on database.
 func OpenConnection(databaseConfig *DatabaseConfig) (err error) {
 	Database, err = sqlx.Connect(databaseConfig.GetDriver(), databaseConfig.GetConnectionString())
+	return err
+}
+
+func Open(databaseConfig *DatabaseConfig) (err error) {
+	Database, err = sqlx.Open(databaseConfig.GetDriver(), databaseConfig.GetConnectionString())
 	return err
 }
 
