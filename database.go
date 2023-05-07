04e3e943cb5d361fa8962027cc9180b5a78fd292
Szabó Ármin András <46499638+lonelymous@users.noreply.github.com>
2023-05-07 19:07:16 +0200

    OpenAndCreate function

--- database.go
+++ database.go
@@ -1,6 +1,11 @@
 package godab
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -17,6 +22,45 @@ func OpenConnection(databaseConfig *DatabaseConfig) (err error) {
 func Open(databaseConfig *DatabaseConfig) (err error) {
 	Database, err = sqlx.Open(databaseConfig.GetDriver(), databaseConfig.GetConnectionStringWithoutDatabase())
 	return err
+}
+
+func OpenAndCreate(databaseConfig *DatabaseConfig, filename string) (err error) {
+	err = Open(databaseConfig)
+	if err != nil {
+		return err
+	}
+
+	sqlFile, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	fileScanner := bufio.NewScanner(sqlFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	line := ""
+	for fileScanner.Scan() {
+		l := strings.TrimSpace(fileScanner.Text())
+
+		if line == "" || strings.HasPrefix(l, "--") {
+			continue
+		}
+
+		line = line + l
+
+		if l[len(l)-1:] == ";" {
+			Database.Exec(line)
+			line = ""
+		}
+	}
+
+	err = sqlFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return nil
 }
 
 // Close connection on database //defer it.
