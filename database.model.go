6acc9a69c68d02052c002cc5719d864df788f65d
Szabó Ármin András <46499638+lonelymous@users.noreply.github.com>
2023-05-19 05:44:37 +0200

    add basic docker tags to DatabaseConfig struct

--- database.model.go
+++ database.model.go
@@ -1,10 +1,18 @@
 package godab
 
+import "strconv"
+
 type DatabaseConfig struct {
-	Driver   string `json:"driver"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	// Hostname or IP address
+	Hostname string `json:"hostname" docker:"DATABASE_HOSTNAME"`
+	Port     int    `json:"port" docker:"DATABASE_PORT"`
+	// Database driver
+	Driver string `json:"driver" docker:"DATABASE_DRIVER"`
+	// Database name
+	Name      string `json:"name" docker:"DATABASE_NAME"`
+	Username  string `json:"username" docker:"DATABASE_USERNAME"`
+	Password  string `json:"password" docker:"DATABASE_PASSWORD"`
+	ParseTime bool   `json:"parse_time" docker:"DATABASE_PARSE_TIME"`
 }
 
 func (this *DatabaseConfig) GetDriver() string {
@@ -12,9 +20,30 @@ func (this *DatabaseConfig) GetDriver() string {
 }
 
 func (this *DatabaseConfig) GetConnectionString() string {
-	return this.Username + ":" + this.Password + "@/" + this.Name + "?parseTime=true"
+	return this.Username +
+		":" +
+		this.Password +
+		"@tcp(" +
+		this.Hostname +
+		":" + strconv.Itoa(this.Port) +
+		")/" + this.Name +
+		"?parseTime=" + this.GetParseTime()
 }
 
 func (this *DatabaseConfig) GetConnectionStringWithoutDatabase() string {
-	return this.Username + ":" + this.Password + "@/?parseTime=true"
+	return this.Username +
+		":" +
+		this.Password +
+		"@tcp(" +
+		this.Hostname +
+		":" + strconv.Itoa(this.Port) +
+		")/?parseTime=" + this.GetParseTime()
+}
+
+func (this *DatabaseConfig) GetDatabasePort() string {
+	return ":" + strconv.Itoa(this.Port)
+}
+
+func (this *DatabaseConfig) GetParseTime() string {
+	return strconv.FormatBool(this.ParseTime)
 }
